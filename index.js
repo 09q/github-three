67a0596b1ab6e110d3fc157ddecbc6714ea8e822
V <60380753+09q@users.noreply.github.com>
2021-12-06 11:32:16 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const request = require('request');
+const colors = require('colors')
+
+function letrinhas() {
+	let nickzinhu = '';
+	let abc123 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+	for(var i = 0; i < 3; i++) {
+		nickzinhu = nickzinhu + abc123.charAt(Math.floor(Math.random() * abc123.length));
+	}
+	//console.log(nickzinhu)
+
+request(`https://github.com/${nickzinhu}`, (error, res, body) => {
+	//console.log(body)
+	if(res.statusCode == 200) {
+		console.log('[.]'.brightBlue + ` ${nickzinhu} Unavailable`.grey)
+	} else {
+		console.log('[+]'.brightGreen + ` ${nickzinhu} Available - [ github.com/${nickzinhu} ]`.white)
+	}
+	})
+}
+
+setInterval(() => letrinhas(), 3500)
